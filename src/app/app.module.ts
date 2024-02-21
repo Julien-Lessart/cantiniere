c61be43d058ef9d7e59db3e9896d3c53fb624a69
Demdoum Yanis <demdoum.yanis@hotmail.com>
2024-02-21 10:23:56 +0100

    init filter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterComponentComponent } from './components/filter-component/filter-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponentComponent
   ],
   imports: [
     BrowserModule,
